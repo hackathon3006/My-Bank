0ee86f6764f48a51e55319f3758de30817b105f5
ckale12 <kalechaitali1993@gmail.com>
2019-08-03 15:06:59 +0530

    account summary

--- src/main/java/com/mybank/bank/controller/CustomerController.java
+++ src/main/java/com/mybank/bank/controller/CustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import com.mybank.bank.service.CustomerService;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/customer")
 public class CustomerController {
 
 	@Autowired
