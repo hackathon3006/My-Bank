90d2af54ac93e452ca7f69c6ce7c90920fb31e17
Sagar Gaikwad <sagargaikwad966@gmail.com>
2019-08-03 12:03:54 +0530

    entities finalize

--- src/main/java/com/mybank/bank/entity/Transaction.java
+++ src/main/java/com/mybank/bank/entity/Transaction.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -36,8 +38,11 @@ public class Transaction implements Serializable{
 	
 	private String remarks;
 	
-
+	@ManyToOne
+	@JoinColumn(name = "from_account_id")
 	private Account fromAccount;
 	
+	@ManyToOne
+	@JoinColumn(name = "to_account_id")
 	private Account toAccount;
 }
